c0860e6a003b5cbf15b8d31f0e59716b9ac9ab80
lzadhito <lzadhito@gmail.com>
2023-07-18 10:35:11 +0700

    feat: integrate dummy routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import App from './App.jsx';
+import './index.css';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
-)
+  </QueryClientProvider>
+);
